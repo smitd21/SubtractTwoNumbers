593004a902a1f50dd35bdb3639043ea7daa28ded
smitd21 <smitdesai2130@gmail.com>
2022-04-26 22:24:16 +0530

    first commit

---
+++ SubtractTwoNumbers.java
@@ -0,0 +1,12 @@
+//Java program for substraction two numbers
+public class SubtractTwoNumbers{
+public static void main(String args[]){
+//variable declaration and initialization
+int result,num1=45,num2=78;
+    
+result=num2-num1;//calculate subtraction using equation
+System.out.println("Substraction of integers: "+result);
+//displays result of subtraction
+}
+}
+
